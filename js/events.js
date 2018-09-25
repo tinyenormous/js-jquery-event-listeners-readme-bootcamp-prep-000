d34e080404a4ba22a4d07431053bc707b17878f7
ross daly <rossdaly@gmail.com>
2018-09-25 01:53:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass( "tasty" );
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if (key.which == 71){
+      alert('you pressed the g key.');
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+      alert("Your form is going to be submitted now.")
+  })  
+}
 
 $(document).ready(function(){
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 // call functions here
 
 });
+
